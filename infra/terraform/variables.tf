bd6aeab0e549aa381aa4dd070fe316b3b00b963f
Кирилл Добрынин <kdobrynin@astralinux.ru>
2021-01-21 23:58:22 +0300

    feat: add terraform infra for gitlab

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,25 @@
+
+variable cloud_id {
+    description = "Cloud"
+}
+
+variable folder_id {
+    description = "Folder"
+}
+
+variable zone {
+    description = "Zone"
+    default = "ru-central1-a"
+}
+
+variable public_key_path {
+    description = "Path to public key used for ssh access"
+}
+
+variable subnet_id {
+    description = "Subnet"
+}
+
+variable image_id {
+    description = "VM image"
+}
