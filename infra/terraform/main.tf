0f50384a5d688366099bb4bf672568dc7f5b9d0a
Кирилл Добрынин <kdobrynin@astralinux.ru>
2021-01-24 04:29:42 +0300

    feat: add monitoring instance

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -21,6 +21,33 @@ resource "yandex_compute_instance" "gitlab_instance" {
         initialize_params {
         image_id = var.image_id
         size = 50
+        type = "network-ssd"
+        }
+    }
+
+    network_interface {
+        subnet_id = var.subnet_id
+        nat       = true
+    }
+}
+
+resource "yandex_compute_instance" "monitoring_instance" {
+    name = "monitoring"
+
+    resources {
+    cores  = 2
+    memory = 4
+    }
+
+    metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+    }
+
+    boot_disk {
+        initialize_params {
+        image_id = var.image_id
+        size = 50
+        type = "network-ssd"
         }
     }
 
