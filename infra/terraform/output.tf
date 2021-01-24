6a1834bb1ac12639e30c5661c646e255c08c0574
Кирилл Добрынин <kdobrynin@astralinux.ru>
2021-01-24 14:37:22 +0300

    feat: add docker role to monitoring instance

--- infra/terraform/output.tf
+++ infra/terraform/output.tf
@@ -1,7 +1,8 @@
 resource "local_file" "AnsibleInventory" {
   content = templatefile("inventory.tmpl",
   {
-    instance_ip = yandex_compute_instance.gitlab_instance[*].network_interface.0.nat_ip_address,
+    instance_ip_gitlab = yandex_compute_instance.gitlab_instance[*].network_interface.0.nat_ip_address,
+    instance_ip_monitoring = yandex_compute_instance.monitoring_instance[*].network_interface.0.nat_ip_address,
   }
   )
   filename = "../ansible/environments/prod/inventory"
