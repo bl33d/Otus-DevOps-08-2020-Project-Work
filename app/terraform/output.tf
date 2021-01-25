0fe76a0f1718a112d7e68077ce9ae9f1adf2dfad
Илья Голиков <igolikov@astralinux.ru>
2021-01-25 21:41:54 +0300

    feat: add monitoring in inventory generator

--- app/terraform/output.tf
+++ app/terraform/output.tf
@@ -1,7 +1,12 @@
+data "yandex_compute_instance" "monitoring_instance" {
+  name = "monitoring"
+}
+
 resource "local_file" "AnsibleInventory" {
   content = templatefile("inventory.tmpl",
   {
-    instance_ip = yandex_compute_instance.app_instance[*].network_interface.0.nat_ip_address,
+    instance_app_ip = yandex_compute_instance.app_instance[*].network_interface.0.nat_ip_address,
+    instance_monitoring_ip = data.yandex_compute_instance.monitoring_instance[*].network_interface.0.nat_ip_address
   }
   )
   filename = "../ansible/environments/prod/inventory"
