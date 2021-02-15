56631e583d68147edcd13bed9a782f172fc0ff0b
Илья Голиков <igolikov@astralinux.ru>
2021-02-15 19:36:12 +0300

    add logging system

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -57,3 +57,28 @@ resource "yandex_compute_instance" "monitoring_instance" {
         nat       = true
     }
 }
+
+resource "yandex_compute_instance" "logging_instance" {
+    name = "logging"
+
+    resources {
+    cores  = 2
+    memory = 4
+    }
+
+    metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+    }
+
+    boot_disk {
+        initialize_params {
+        image_id = var.image_id
+        size = 50
+        }
+    }
+
+    network_interface {
+        subnet_id = var.subnet_id
+        nat       = true
+    }
+}
--- infra/terraform/output.tf
+++ infra/terraform/output.tf
@@ -3,6 +3,7 @@ resource "local_file" "AnsibleInventory" {
   {
     instance_ip_gitlab = yandex_compute_instance.gitlab_instance[*].network_interface.0.nat_ip_address,
     instance_ip_monitoring = yandex_compute_instance.monitoring_instance[*].network_interface.0.nat_ip_address,
+    instance_ip_logging = yandex_compute_instance.logging_instance[*].network_interface.0.nat_ip_address,
   }
   )
   filename = "../ansible/environments/prod/inventory"
