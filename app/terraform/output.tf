ad4c1ea9646f28f13b777c9a9a3013e78b1cf2b9
Илья Голиков <igolikov@astralinux.ru>
2021-02-15 19:36:12 +0300

    add logging system

--- app/terraform/output.tf
+++ app/terraform/output.tf
@@ -2,11 +2,16 @@ data "yandex_compute_instance" "monitoring_instance" {
   name = "monitoring"
 }
 
+data "yandex_compute_instance" "logging_instance" {
+  name = "logging"
+}
+
 resource "local_file" "AnsibleInventory" {
   content = templatefile("inventory.tmpl",
   {
-    instance_app_ip = yandex_compute_instance.app_instance[*].network_interface.0.nat_ip_address,
+    instance_app_ip        = yandex_compute_instance.app_instance[*].network_interface.0.nat_ip_address,
     instance_monitoring_ip = data.yandex_compute_instance.monitoring_instance[*].network_interface.0.nat_ip_address
+    instance_logging_ip    = data.yandex_compute_instance.logging_instance[*].network_interface.0.nat_ip_address,
   }
   )
   filename = "../ansible/environments/prod/inventory"
